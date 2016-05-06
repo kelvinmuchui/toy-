5534520602df8c75afa16e22db408b0ef87fe025
kelvinmuchui <muchuikelvin423@gmail.com>
2016-05-06 19:30:11 +0300

    toy prombles

---
+++ remove_duplicate/remove_duplicate.java
@@ -0,0 +1,7 @@
+// Using Java, remove duplicate characters in a given string keeping only the first occurrences. 
+// For example, if the input is ‘tree traversal’ the output will be "tre avsl". Enjoy:-)
+
+ public class removeDuplicate{
+ 	//code goes here
+
+ }
